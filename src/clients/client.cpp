98dab388abe28481b1af1fb6e999ee3bdc039291
pabin234 <thakurishahisushma60@gmail.com>
2026-02-13 15:32:08 +0000

    gets replace by cn.getline

--- src/clients/client.cpp
+++ src/clients/client.cpp
@@ -5,49 +5,49 @@
 #include "client.h"
 using namespace std;
 
+void Client::enterDetails()
+{
+    cout << "Enter Client Name: ";
+    cin.getline(name, sizeof(name));
+    cout << "Enter Client Address: ";
+    cin.getline(address, sizeof(address));
+    cout << "Enter Client Phone: ";
+    cin.getline(phone, sizeof(phone));
+    cout << "Enter Client Email: ";
+    cin.getline(email, sizeof(email));
+}
 
-    void Client::enterDetails()
-    {
-        cout << "Enter Client Name: ";
-        gets(name);
-        cout << "Enter Client Address: ";
-        gets(address);
-        cout << "Enter Client Phone: ";
-        gets(phone);
-        cout << "Enter Client Email: ";
-        gets(email);
-    }
+void Client::viewDetails()
+{
+    cout << "\nClient Details:\n";
+    cout << "Name: " << name << "\n";
+    cout << "Address: " << address << "\n";
+    cout << "Phone: " << phone << "\n";
+    cout << "Email: " << email << "\n";
+}
 
-    void Client::viewDetails()
+void Client::menu()
+{
+    int choice;
+    cout << "\n";
+    cout << "||||||||||||||||||||||||||||||||||||||||||||||\n";
+    cout << "||          Client Management Menu          ||\n";
+    cout << "||||||||||||||||||||||||||||||||||||||||||||||\n";
+    cout << "1. Add Client\n";
+    cout << "2. View Client\n";
+    cout << "3. Back to Main Menu\n";
+    cout << "Enter choice:";
+    cin >> choice;
+    fflush(stdin);
+    switch (choice)
     {
-        cout << "\nClient Details:\n";
-        cout << "Name: " << name << "\n";
-        cout << "Address: " << address << "\n";
-        cout << "Phone: " << phone << "\n";
-        cout << "Email: " << email << "\n";
+    case 1:
+        enterDetails();
+        break;
+    case 2:
+        viewDetails();
+        break;
+    case 3:
+        break;
     }
-    
-    void Client::menu()
-    {
-    	int choice;
-        cout << "\n";
-        cout << "||||||||||||||||||||||||||||||||||||||||||||||\n";
-        cout << "||          Client Management Menu          ||\n";
-        cout << "||||||||||||||||||||||||||||||||||||||||||||||\n";
-        cout << "1. Add Client\n";
-        cout << "2. View Client\n";
-        cout << "3. Back to Main Menu\n";
-        cout << "Enter choice:";
-        cin >>choice;
-        fflush(stdin);
-        switch (choice){
-        	case 1:
-        		enterDetails();
-                break;
-        	case 2:
-        		viewDetails();
-                break;
-        	case 3:
-                break;
-		}
-    }
+}
