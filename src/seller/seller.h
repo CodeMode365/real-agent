d916f7d3ebfeec9c3b04d0e1d048f76ef60dc791
pabin234 <thakurishahisushma60@gmail.com>
2026-02-13 15:35:17 +0000

    seller code regen

---
+++ src/seller/seller.h
@@ -0,0 +1,32 @@
+#pragma once
+
+struct SellerDetails
+{
+    int id;
+    char name[100];
+    char contact[50];
+    char address[150];
+};
+
+class Seller
+{
+private:
+    SellerDetails sellers[100];
+    char fileName[100];
+
+    void readSellersFromFile();
+    void writeSellerToFile(SellerDetails seller);
+
+public:
+    Seller();
+
+    void menu();
+
+    void addSeller();
+
+    void viewSellers();
+
+    SellerDetails getSellerById(int id);
+
+    void deleteSellerById(int id);
+};
